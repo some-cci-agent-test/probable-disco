8a75c805059253c55d6dd0289ddf211f152a7487
Keaton Laney <keatonus@gmail.com>
2025-08-15 15:31:38 -0700

    trigger pipeline 23: add trailing space to Subtract comment

--- pkg/calc/calc.go
+++ pkg/calc/calc.go
@@ -5,7 +5,7 @@ func Add(a, b int) int {
 	return a + b
 }
 
-// Subtract returns the difference of a and b.
+// Subtract returns the difference of a and b. 
 func Subtract(a, b int) int {
 	return a - b
 }
